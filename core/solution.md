424ecb25ada967f886d41985d2b6ee344dddc2f4
Malik Warren <malikwarren@Maliks-MacBook-Pro-2.local>
2022-06-16 01:12:31 -0400

    added solution doc, added some styling, updated default value of views

---
+++ core/solution.md
@@ -0,0 +1,25 @@
+# Project Purple Cow - Fearless Application
+
+## General info
+This project is a React Application that uses CounterAPI to track the number of visits on the web application. This application increments the view counter by 1 for each time the button is click by the user.
+
+Find CounterAPI documentation here: https://countapi.xyz/
+
+## Technologies
+React
+Chrome Browser
+
+## Setup
+To run this project:
+Download from Git repo
+install it locally using
+### `npm install` in the root directory:
+
+Start the application:
+### `npm start`
+
+Localhost port is defaulted to 3000, but can be changed in package.json file by changing the start scripts.
+### Example: "start": "PORT=3000 react-scripts start"
+
+Application can be accessed via chrome browser:
+http://localhost:3000
--- core/src/components/EventBind.js
+++ core/src/components/EventBind.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
+const Button = styled.button`
+  background-color: #5715d1;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+`;
 const countapi = require('countapi-js');
 
 class EventBind extends Component {
@@ -7,7 +17,7 @@ class EventBind extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            views: "click to add views"
+            views: 0
         }
     }
     websiteVisits(response) {
@@ -24,7 +34,7 @@ class EventBind extends Component {
                 <div>
                     <h1>This site has been visited {this.state.views} times!</h1>
                 </div>
-                <button onClick={this.websiteVisits.bind(this)}>Add Views</button>
+                <Button button onClick={this.websiteVisits.bind(this)}>Add Views</Button>
             </div>
         )
     }
